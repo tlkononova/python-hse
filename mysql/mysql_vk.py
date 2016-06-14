c9ee82e80cbea5e76c759f6586dedaf19c7e6350
tlkononova <tkononova@gmail.com>
2016-06-14 03:53:18 +0300

    change table names

--- mysql/mysql_vk.py
+++ mysql/mysql_vk.py
@@ -23,19 +23,19 @@ def main():
     path = ''
     with open(path+'usermeta_97.csv', 'r', encoding='utf-8') as csvf:
             
-        cur.execute('create table VKmeta (userid INT(10), birthdate VARCHAR(10), sex INT(1), PRIMARY KEY(userid));')       
+        cur.execute('create table VK_meta (userid INT(10), birthdate VARCHAR(10), sex INT(1), PRIMARY KEY(userid));')       
         
         meta = csv.DictReader(csvf, delimiter='\t')
         
         for row in meta:
             string="'"+str(row['userid'])+"','"+str(row['birthdate']+"','"+str(row['sex'])+"'")
             
-            cur.execute('insert into VKmeta (userid, birthdate, sex) value ('+string+');')
+            cur.execute('insert into VK_meta (userid, birthdate, sex) value ('+string+');')
     
     
     with open(path+'alluserwall_97.csv', 'r', encoding='utf-8') as csvf:
     
-        cur.execute('create table VKwall (userid INT(10), date VARCHAR(20), text VARCHAR(10000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')       
+        cur.execute('create table VK_wall (userid INT(10), date VARCHAR(20), text VARCHAR(10000)) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;')       
         
         wall = csv.DictReader(csvf, delimiter='\t')
         
@@ -44,7 +44,7 @@ def main():
             string="'"+str(row['userid'])+"','"+str(row['date']+"','"+str(row['text'])+"'")
             
             try:
-                cur.execute('insert into VKwall (userid, date, text) value ('+string+');')
+                cur.execute('insert into VK_wall (userid, date, text) value ('+string+');')
             except:
                 print(str(row['text']))
     
